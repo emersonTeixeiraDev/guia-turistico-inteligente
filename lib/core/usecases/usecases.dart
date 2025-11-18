322267bf050f941fba70752c8d71678bcca754d2
Emerson Teixeira <emersonteixeiraweb@gmail.com>
2025-11-18 14:48:48 -0300

    fix: ajusta nome do parametro generico no UseCase para evitar warning

--- lib/core/usecases/usecases.dart
+++ lib/core/usecases/usecases.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import '../error/failures.dart';
 
-abstract class UseCases<Type, Params> {
-  Future<Either<Failure, Type>> call(Params params);
+abstract class UseCases<T, Params> {
+  Future<Either<Failure, T>> call(Params params);
 }
 
 class NoParams {}
